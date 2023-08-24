3d8015211aaaa3d868ddc944c693b34a2180c5e4
Abilashsp <139426960+Abilashsp@users.noreply.github.com>
2023-08-24 17:09:41 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-red-800 text-green-600">
+      
+    </div>
+  )
+}
