6e9defd9d23c45f1cf568a95462197c2e9189f8f
Abilashsp <abilashsp07@gmail.com>
2023-09-29 19:41:09 +0530

    include  the sample data

--- src/components/Fields/TextBox.js
+++ src/components/Fields/TextBox.js
@@ -1,30 +1,31 @@
-import{Formik,Form,Field,ErrorMessage}from "formik"
-import { useState } from "react";
+import { useRef } from "react";
 
 const TextBox = (props) => {
-  const[inputvalues,setinputvalues]=useState([]);
-
-
-const handleinput=(data)=>{
-
-setinputvalues(data)
-console.log(inputvalues)
- 
-}
-
-
-
+  const inputRef = useRef(null);
 
   const {
+    className,
     "Field Name": Field_Name,
     "Data Type": data_type,
+    type = "text",
     Validate,
     Tooltip,
     Prompt,
-    "Sample Value":sample_value,
-
+    section,
+    record,
   } = props;
-  
+  let _value = "";
+
+  if (Field_Name && section && record && record[section]) {
+    _value = record[section][Field_Name];
+
+    if (_value === "Yes") _value = true;
+    if (_value === "No") _value = false;
+    console.log(Field_Name, _value);
+
+    // TODO : Need move this to Formik
+    // inputRef.current.value = _value;
+  }
   return (
     <>
       <label
@@ -32,43 +33,29 @@ console.log(inputvalues)
         className="block text-sm font-medium leading-6 text-gray-900"
       >
         {Field_Name}
-        {Validate==="Mandatory" && (
+        {Validate && (
           <span className="pl-1 text-xs text-red-500" title={Tooltip}>
             *
           </span>
         )}
       </label>
-      <Formik
-      initialValues={{}}
-      validate={(values)=>{
-      handleinput(values)
-      }
-     
-    }
-      
-      
-      >
-        <Form onSubmit={handleinput}>
       <div className="mt-1">
         <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-          
-          <Field
-            type="text"
-            name={Field_Name}
+          <input
+            ref={inputRef}
             id={Field_Name}
-            value={sample_value}
-            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="text"
+            value={_value}
+            name={Field_Name}
+            className={` block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
             placeholder={Prompt}
-            // required
           />
-       
         </div>
+        {/* {_value} */}
         {Tooltip && Tooltip.toLowerCase() !== Field_Name.toLowerCase() && (
           <p className="mt-1 ml-2 text-xs text-blue-500">{Tooltip}</p>
         )}
       </div>
-      </Form>
-      </Formik>
     </>
   );
 };
--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -1,5 +1,57 @@
 import CheckBox from "./CheckBox";
 import TextBox from "./TextBox";
 import Select from "./Select";
-import RadioBox from "./RadioBox";
-export { CheckBox, TextBox, Select,RadioBox };
+import { Herr_Von_Muellerhoff } from "next/font/google";
+
+const NumberBox = (props) => (
+  <TextBox className="w-36" type="number" min="0" {...props} />
+);
+const Email = (props) => <TextBox type="email" {...props} />;
+const UrlField = (props) => <TextBox type="url" {...props} />;
+const DateField = (props) => <TextBox type="date" {...props} />;
+const FileField = (props) => <TextBox type="file" {...props} />;
+const TimeField = (props) => <TextBox type="time" {...props} />;
+const WeekField = (props) => <TextBox type="week" {...props} />;
+
+export {
+  CheckBox,
+  TextBox,
+  Select,
+  NumberBox,
+  Email,
+  UrlField,
+  DateField,
+  TimeField,
+  FileField,
+  WeekField,
+};
+
+// https://www.behance.net/gallery/177185839/Restaurant-POS-System-(Retail-Management-System)?tracking_source=search_projects|design+system
+// https://www.behance.net/gallery/127252161/Data-tables-design-system-Free-set-for-Figma?tracking_source=search_projects|design+system
+// Ref : https://www.behance.net/gallery/161215951/Eos-Design-System-UI-kit-Library
+// Button States
+// Enabled
+// Disabled
+// Hover
+// Pressed
+// Processing
+
+// Checkbox
+// unselected
+// selected
+// interminate
+// inactive unselected
+// inactive selected
+// inactive interminate
+
+//  Radio Button
+// unselcted
+// selected
+// inactive unselcted
+// inactive  selected
+
+//  Switch
+// inactive
+// active
+// inactive disabled
+// active disabled
